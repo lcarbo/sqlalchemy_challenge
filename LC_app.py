ae66afb72d6aad782870cc70f388fea64719fac5
Lily Carbonara <lilycarbonara@dans-mbp-2.myfiosgateway.com>
2021-01-31 17:41:47 -0500

    progress on app

--- LC_app.py
+++ LC_app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -33,7 +34,8 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session=Session(engine)
-    precip_result=session.query(measures.date, measures.prcp).all()
+    oneyr_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+    precip_result=session.query(measures.date, measures.prcp).filter(measures.date >= oneyr_ago).order_by(measures.date.asc()).all()
     session.close()
 
     precip_list= []
@@ -42,9 +44,60 @@ def precipitation():
         precip_dict={}
         precip_dict["date"] = date
         precip_dict["prcp"] = prcp
-        precip_list.append(precip_dict)
+        precip_list.append(precip_dict) 
 
-    return jsonify(precip_dict)
+    return jsonify(precip_list)
+
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session=Session(engine)
+    station_result=session.query(measures.station).distinct().all()
+    session.close()
+
+    station_list= []
+    
+    for each in station_result: 
+        station_list.append(each)  
+
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session=Session(engine)
+    oneyr_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+    active_annual = session.query(measures.date, measures.tobs).filter(measures.station =='USC00519281').filter(measures.date >= oneyr_ago).order_by(measures.date.asc()).all()
+
+    session.close()
+
+    active_list = []
+    
+    for date, tobs in active_annual: 
+            active_dict={}
+            active_dict["date"] = date
+            active_dict["tobs"] = tobs
+            active_list.append(active_dict) 
+
+    return jsonify(active_list)
+
+@app.route("/api/v1.0/<start_date>")
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def date_range(start_date= none, end_date= none):
+
+    stats = [func.min(measures.tobs), func.avg(measures.tobs), func.max(measures.tobs)]
+
+    if end == none: 
+        range_results = session.query(*stats).filter(measures.date >= start_date).all()
+    range_tobs = list(np.ravel(range_results))
+    return jsonify (range_tobs)
+
+
+
+
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
